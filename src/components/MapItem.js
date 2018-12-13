a43a53ad02d0d188db3be22d1b06a2b532730f20
pennywise97 <aishwaryasolanki1997@gmail.com>
2018-12-13 15:32:05 +0530

    Components connected

---
+++ src/components/MapItem.js
@@ -0,0 +1,9 @@
+import React, {Component} from 'react';
+
+export default class MapItem extends Component{
+  render(){
+    return(
+      <p>Hi</p>
+    );
+  }
+}
---
+++ src/components/MapList.js
@@ -0,0 +1,13 @@
+import React, {Component} from 'react';
+import MapItem from './MapItem';
+
+export default class MapList extends Component{
+  render(){
+    return(
+      <div>
+        <p>Hey</p>
+        <MapItem/>
+      </div>
+    );
+  }
+}
