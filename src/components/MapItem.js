c7e18c2cc2c14e160c0f6acc5da9ac4a2f52ef3c
pennywise97 <aishwaryasolanki1997@gmail.com>
2018-12-13 18:16:01 +0530

    Update item

--- src/components/MapItem.js
+++ src/components/MapItem.js
@@ -1,15 +1,19 @@
 import React, {Component} from 'react';
 
 export default class MapItem extends Component{
+
   render(){
+
     return(
+
       <button
-      className="mapButton" 
+      className="mapButton"
       tabIndex="0"
       onKeyPress={this.props.openInfoWindow.bind(this, this.props.data.marker)}
       onClick={this.props.openInfoWindow.bind(this, this.props.data.marker)}>
-      {this.props.data.longname}
+      {this.props.data.fullname}
       </button>
+      
     );
   }
 }
